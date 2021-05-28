a5056ad23b6c8cf61b624cec7945b4c77d1b957b
Adrian Perdomo <82109017+aperdomoll90@users.noreply.github.com>
2021-05-28 13:56:09 -0400

    added cors

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,10 @@
-
+const cors =require('cors')
 const functions = require('firebase-functions');
 const express = require('express');
 const { getAllUsers, getUserById, newUser,updateUser,deleteUser } = require('./src/users/');
 
 const app = express();
+app.use(cors())
 
 app.post('/users', newUser); //create New user
 app.get('/users/:userId', getUserById); //get user by ID
@@ -11,8 +12,8 @@ app.get('/users', getAllUsers); //get all users
 app.patch('/users/:userId', updateUser) // update User by id
 app.delete('/users/:userId', deleteUser) // delete User by id
 
-// app.get('/logs/:useId', getAllLogs); //get all logs by user
-// app.post('/logs/:useId', newLog); //create New log
+app.get('/logs/:userId', getAllLogs); //get all logs by user
+app.post('/logs/:userId', newLog); //create New log
 
 
 
